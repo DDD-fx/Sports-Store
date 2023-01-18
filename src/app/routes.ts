1ce002827cafdb3785daaf863574d1db175bc221
ddd-fx <d.khoroshkin@gmail.com>
2023-01-18 12:32:39 +0600

    add routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-const routes: Routes = [];
+export const ROUTES: Routes = [
+  {
+    path: 'store',
+    loadComponent: () =>
+      import('./store/store.component').then((mod) => mod.StoreComponent),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./store/cart-detail/cart-detail.component').then(
+        (mod) => mod.CartDetailComponent
+      ),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./store/checkout/checkout.component').then(
+        (mod) => mod.CheckoutComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/store',
+  },
+];
